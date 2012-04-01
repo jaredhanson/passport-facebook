1b3f603af376eb707b52032ddeee8c18402ff630
Jared Hanson <jaredhanson@gmail.com>
2012-03-31 17:54:15 -0700

    Wrap error for more informative messages.

--- test/strategy-test.js
+++ test/strategy-test.js
@@ -105,6 +105,9 @@ vows.describe('FacebookStrategy').addBatch({
       'should error' : function(err, req) {
         assert.isNotNull(err);
       },
+      'should wrap error in InternalOAuthError' : function(err, req) {
+        assert.equal(err.constructor.name, 'InternalOAuthError');
+      },
       'should not load profile' : function(err, profile) {
         assert.isUndefined(profile);
       },
