1e74b7b5019ec285f097736b51606cd7717726cd
Jared Hanson <jaredhanson@gmail.com>
2013-08-15 13:07:16 -0700

    Error in response to server misconfiguration.

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -14,7 +14,7 @@ describe('Strategy', function() {
     expect(strategy.name).to.equal('facebook');
   });
   
-  describe('handling a return request in which authorization has failed with an error', function() {
+  describe('handling a return request in which authorization was denied by user', function() {
     var info;
   
     before(function(done) {
@@ -25,16 +25,42 @@ describe('Strategy', function() {
         })
         .req(function(req) {
           req.query = {};
-          req.query.error_code = '901';
-          req.query.error_message = 'This app is in sandbox mode.  Edit the app configuration at http://developers.facebook.com/apps to make the app publicly visible.';
+          req.query.error = 'access_denied';
+          req.query.error_code = '200';
+          req.query.error_description  = 'Permissions error';
+          req.query.error_reason = 'user_denied';
         })
         .authenticate();
     });
   
     it('should fail with info', function() {
       expect(info).to.not.be.undefined;
-      expect(info.code).to.equal(901);
-      expect(info.message).to.equal('This app is in sandbox mode.  Edit the app configuration at http://developers.facebook.com/apps to make the app publicly visible.');
+      expect(info.message).to.equal('Permissions error');
+    });
+  });
+  
+  describe('handling a return request in which authorization has failed with an error', function() {
+    var err;
+  
+    before(function(done) {
+      chai.passport(strategy)
+        .error(function(e) {
+          err = e;
+          done();
+        })
+        .req(function(req) {
+          req.query = {};
+          req.query.error_code = '901';
+          req.query.error_message = 'This app is in sandbox mode.  Edit the app configuration at http://developers.facebook.com/apps to make the app publicly visible.';
+        })
+        .authenticate();
+    });
+  
+    it('should error', function() {
+      expect(err.constructor.name).to.equal('FacebookAuthorizationError');
+      expect(err.message).to.equal('This app is in sandbox mode.  Edit the app configuration at http://developers.facebook.com/apps to make the app publicly visible.');
+      expect(err.code).to.equal(901);
+      expect(err.status).to.equal(500);
     });
   });
   
