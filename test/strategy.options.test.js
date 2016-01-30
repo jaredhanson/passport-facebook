253d047769725a2a77ad4db55e7147752a105861
Jared Hanson <jaredhanson@gmail.com>
2016-01-29 19:20:09 -0800

    Move appsecret_proof test case.

--- test/strategy.options.test.js
+++ test/strategy.options.test.js
@@ -5,42 +5,6 @@ var FacebookStrategy = require('../lib/strategy');
 
 
 describe('Strategy#userProfile', function() {
-  
-  describe('with default URL and proof enabled', function() {
-    var strategy = new FacebookStrategy({
-        clientID: 'ABC123',
-        clientSecret: 'secret',
-        enableProof: true
-      },
-      function() {});
-  
-    // mock
-    strategy._oauth2.get = function(url, accessToken, callback) {
-      if (url != 'https://graph.facebook.com/v2.2/me?appsecret_proof=e941110e3d2bfe82621f0e3e1434730d7305d106c5f68c87165d0b27a4611a4a') { return callback(new Error('incorrect url argument')); }
-      if (accessToken != 'token') { return callback(new Error('incorrect token argument')); }
-    
-      var body = '{"id":"500308595","name":"Jared Hanson","first_name":"Jared","last_name":"Hanson","link":"http:\\/\\/www.facebook.com\\/jaredhanson","username":"jaredhanson","gender":"male","email":"jaredhanson\\u0040example.com"}';
-      callback(null, body, undefined);
-    };
-    
-    describe('loading profile', function() {
-      var profile;
-    
-      before(function(done) {
-        strategy.userProfile('token', function(err, p) {
-          if (err) { return done(err); }
-          profile = p;
-          done();
-        });
-      });
-    
-      it('should parse profile', function() {
-        expect(profile.provider).to.equal('facebook');
-        expect(profile.id).to.equal('500308595');
-        expect(profile.username).to.equal('jaredhanson');
-      });
-    });
-  });
     
   describe('with profile URL option', function() {
     var strategy = new FacebookStrategy({
--- test/strategy.profile.test.js
+++ test/strategy.profile.test.js
@@ -55,6 +55,39 @@ describe('Strategy#userProfile', function() {
     });
   }); // fetched from default endpoint
   
+  describe('fetched from default endpoint, with appsecret_proof', function() {
+    var strategy = new FacebookStrategy({
+        clientID: 'ABC123',
+        clientSecret: 'secret',
+        enableProof: true
+      }, function() {});
+  
+    strategy._oauth2.get = function(url, accessToken, callback) {
+      if (url != 'https://graph.facebook.com/v2.2/me?appsecret_proof=e941110e3d2bfe82621f0e3e1434730d7305d106c5f68c87165d0b27a4611a4a') { return callback(new Error('incorrect url argument')); }
+      if (accessToken != 'token') { return callback(new Error('incorrect token argument')); }
+    
+      var body = '{"id":"500308595","name":"Jared Hanson","first_name":"Jared","last_name":"Hanson","link":"http:\\/\\/www.facebook.com\\/jaredhanson","username":"jaredhanson","gender":"male","email":"jaredhanson\\u0040example.com"}';
+      callback(null, body, undefined);
+    };
+    
+
+    var profile;
+  
+    before(function(done) {
+      strategy.userProfile('token', function(err, p) {
+        if (err) { return done(err); }
+        profile = p;
+        done();
+      });
+    });
+  
+    it('should parse profile', function() {
+      expect(profile.provider).to.equal('facebook');
+      expect(profile.id).to.equal('500308595');
+      expect(profile.username).to.equal('jaredhanson');
+    });
+  });
+  
   describe('error caused by invalid token', function() {
     var strategy =  new FacebookStrategy({
         clientID: 'ABC123',
