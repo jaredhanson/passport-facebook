56ae3e567ba910cf1e32330f9f2d3817025fdd2a
Jared Hanson <jaredhanson@gmail.com>
2016-01-29 19:53:12 -0800

    Mover authorization parameter test cases.

--- test/strategy.authorizationparams.test.js
+++ test/strategy.authorizationparams.test.js
@@ -7,48 +7,8 @@ var chai = require('chai')
 
 describe('Strategy', function() {
 
-  var strategy = new FacebookStrategy({
-      clientID: 'ABC123',
-      clientSecret: 'secret'
-    },
-    function() {});
-
-  describe('handling a request to be redirected with display param', function() {
-    var url;
-
-    before(function(done) {
-      chai.passport.use(strategy)
-        .redirect(function(u) {
-          url = u;
-          done();
-        })
-        .req(function(req) {
-        })
-        .authenticate({ display: 'mobile' });
-    });
-
-    it('should be redirected', function() {
-      expect(url).to.equal('https://www.facebook.com/v2.2/dialog/oauth?display=mobile&response_type=code&redirect_uri=&client_id=ABC123');
-    });
-  });
-
   describe('handling a request to be redirected with reauthorization params', function() {
-    var url;
-
-    before(function(done) {
-      chai.passport.use(strategy)
-        .redirect(function(u) {
-          url = u;
-          done();
-        })
-        .req(function(req) {
-        })
-        .authenticate({ authType: 'reauthenticate', authNonce: 'foo123' });
-    });
-
-    it('should be redirected', function() {
-      expect(url).to.equal('https://www.facebook.com/v2.2/dialog/oauth?auth_type=reauthenticate&auth_nonce=foo123&response_type=code&redirect_uri=&client_id=ABC123');
-    });
+    
   });
 
 });
--- test/strategy.test.js
+++ test/strategy.test.js
@@ -26,6 +26,56 @@ describe('Strategy', function() {
       }).to.throw(Error);
     });
   })
+
+  describe('authorization request with display parameter', function() {
+    var strategy = new FacebookStrategy({
+        clientID: 'ABC123',
+        clientSecret: 'secret'
+      }, function() {});
+    
+    
+    var url;
+
+    before(function(done) {
+      chai.passport.use(strategy)
+        .redirect(function(u) {
+          url = u;
+          done();
+        })
+        .req(function(req) {
+        })
+        .authenticate({ display: 'mobile' });
+    });
+
+    it('should be redirected', function() {
+      expect(url).to.equal('https://www.facebook.com/v2.2/dialog/oauth?display=mobile&response_type=code&redirect_uri=&client_id=ABC123');
+    });
+  });
+  
+  describe('authorization request with reauthorization parameters', function() {
+    var strategy = new FacebookStrategy({
+        clientID: 'ABC123',
+        clientSecret: 'secret'
+      }, function() {});
+    
+    
+    var url;
+
+    before(function(done) {
+      chai.passport.use(strategy)
+        .redirect(function(u) {
+          url = u;
+          done();
+        })
+        .req(function(req) {
+        })
+        .authenticate({ authType: 'reauthenticate', authNonce: 'foo123' });
+    });
+
+    it('should be redirected', function() {
+      expect(url).to.equal('https://www.facebook.com/v2.2/dialog/oauth?auth_type=reauthenticate&auth_nonce=foo123&response_type=code&redirect_uri=&client_id=ABC123');
+    });
+  });
   
   describe('failure caused by user denying request', function() {
     var strategy = new FacebookStrategy({
